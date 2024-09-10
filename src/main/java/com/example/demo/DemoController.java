37f0d380ce0a427154dd7d40beb1546bb502d9ff
juanmiguel431 <juanmiguel431@gmail.com>
2024-09-09 21:53:03 -0400

    setter injection.

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,20 +1,28 @@
 package com.example.demo;
 
 import com.example.util.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
-    private final Coach coach;
+    private Coach coach;
+//    private final Coach coach;
+//
+//    public DemoController(Coach coach) {
+//        this.coach = coach;
+//    }
 
-    public DemoController(Coach coach) {
+    @Autowired
+    public void setCoach(Coach coach) {
         this.coach = coach;
     }
 
     @GetMapping("/daily-workout")
     public String getDailyWorkout() {
+
         return coach.getDailyWorkout();
     }
 }
