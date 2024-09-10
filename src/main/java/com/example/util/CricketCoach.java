7f2b004b099f718c2c5ae7297c88f27052c4de39
juanmiguel431 <juanmiguel431@gmail.com>
2024-09-09 23:12:37 -0400

    lazy-initialization=true - Creates the objects only when required.

--- src/main/java/com/example/util/CricketCoach.java
+++ src/main/java/com/example/util/CricketCoach.java
@@ -1,11 +1,17 @@
 package com.example.util;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
+@Lazy
 public class CricketCoach implements Coach {
+
+    public CricketCoach() {
+        System.out.println("In constructor " + this.getClass().getSimpleName());
+    }
 
     @Override
     public String getDailyWorkout() {
--- src/main/java/com/example/util/TrackCoach.java
+++ src/main/java/com/example/util/TrackCoach.java
@@ -1,9 +1,15 @@
 package com.example.util;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 public class TrackCoach implements Coach {
+
+    public TrackCoach() {
+        System.out.println("In constructor " + this.getClass().getSimpleName());
+    }
 
     @Override
     public String getDailyWorkout() {
