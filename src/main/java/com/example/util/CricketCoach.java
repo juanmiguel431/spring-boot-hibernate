007d73478e91bf3efb97179d3159c369323f422d
juanmiguel431 <juanmiguel431@gmail.com>
2024-09-10 11:01:58 -0400

    PostConstruct and PreDestroy lifecycle methods

--- src/main/java/com/example/util/CricketCoach.java
+++ src/main/java/com/example/util/CricketCoach.java
@@ -1,5 +1,7 @@
 package com.example.util;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
@@ -16,5 +18,15 @@ public class CricketCoach implements Coach {
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes. Cricket Coach.";
+    }
+
+    @PostConstruct
+    public void myInitializationStuff(){
+        System.out.println("My initialization stuff " + this.getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void myDestroyStuff(){
+        System.out.println("My destroying stuff " + this.getClass().getSimpleName());
     }
 }
--- src/main/java/com/example/util/TrackCoach.java
+++ src/main/java/com/example/util/TrackCoach.java
@@ -1,5 +1,7 @@
 package com.example.util;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
@@ -14,5 +16,15 @@ public class TrackCoach implements Coach {
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15 minutes. Track Coach.";
+    }
+
+    @PostConstruct
+    public void myInitializationStuff(){
+        System.out.println("My initialization stuff " + this.getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void myDestroyStuff(){
+        System.out.println("My destroying stuff " + this.getClass().getSimpleName());
     }
 }
