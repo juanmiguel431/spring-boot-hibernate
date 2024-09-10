c5300472a7d0d3ec905f68a92fa652752d3ae3b4
juanmiguel431 <juanmiguel431@gmail.com>
2024-09-09 22:28:03 -0400

    The use of @Qualifier when there is more than one class that implement certain interface.

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -2,27 +2,27 @@ package com.example.demo;
 
 import com.example.util.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
-    private Coach coach;
+    private final Coach coach;
 
 //    @Autowired
 //    private Coach coach2;
 
-//    private final Coach coach;
-//
-//    public DemoController(Coach coach) {
-//        this.coach = coach;
-//    }
-
     @Autowired
-    public void setCoach(Coach coach) {
+    public DemoController(@Qualifier("trackCoach") Coach coach) {
         this.coach = coach;
     }
+
+//    @Autowired
+//    public void setCoach(Coach coach) {
+//        this.coach = coach;
+//    }
 
     @GetMapping("/daily-workout")
     public String getDailyWorkout() {
--- src/main/java/com/example/util/TrackCoach.java
+++ src/main/java/com/example/util/TrackCoach.java
@@ -3,10 +3,10 @@ package com.example.util;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CricketCoach implements Coach {
+public class TrackCoach implements Coach {
 
     @Override
     public String getDailyWorkout() {
-        return "Practice fast bowling for 15 minutes. Cricket Coach.";
+        return "Practice fast bowling for 15 minutes. Track Coach.";
     }
 }
